49f78ea5fb5f5a0ac99e63bdf180225de268cc52
Ricardo Aires <ricardoaamb@usp.br>
2022-10-17 16:24:41 -0300

    Versao final sem bonus

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ let sobremesa;
 let a = 0;
 let b = 0;
 let c = 0;
+let x = 0;
+let y = 0;
+let z = 0;
+let total = 0;
 
 
-   
+
 
 
 
@@ -17,6 +21,8 @@ function escolherFrango() {
     document.getElementById("frango3").style.borderColor = "white";
     prato = "Frango";
     a = 1;
+    x = 14.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -29,6 +35,8 @@ function escolherFrango2() {
     document.getElementById("frango3").style.borderColor = "white";
     prato = "Frango2";
     a = 1;
+    x = 13.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -41,6 +49,8 @@ function escolherFrango3() {
     document.getElementById("frango").style.borderColor = "white";
     prato = "Frango3";
     a = 1;
+    x = 12.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -53,6 +63,8 @@ function escolherCoca() {
     document.getElementById("coca3").style.borderColor = "white";
     bebida = "coca";
     b = 1;
+    y = 4.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -65,6 +77,8 @@ function escolherCoca2() {
     document.getElementById("coca3").style.borderColor = "white";
     bebida = "coca2";
     b = 1;
+    y = 3.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -77,6 +91,8 @@ function escolherCoca3() {
     document.getElementById("coca").style.borderColor = "white";
     bebida = "coca3";
     b = 1;
+    y = 2.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -89,6 +105,8 @@ function escolherPudim() {
     document.getElementById("pudim3").style.borderColor = "white";
     sobremesa = "pudim"
     c = 1;
+    z = 7.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -101,6 +119,8 @@ function escolherPudim2() {
     document.getElementById("pudim3").style.borderColor = "white";
     sobremesa = "pudim2"
     c = 1;
+    z = 6.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
@@ -113,9 +133,16 @@ function escolherPudim3() {
     document.getElementById("pudim").style.borderColor = "white";
     sobremesa = "pudim3"
     c = 1;
+    z = 5.9;
+    total = x + y + z;
     if (a + b + c == 3) {
         document.getElementById("FecharPedido").style.display = "block";
         document.getElementById("selecioneItens").style.display = "none";
     }
+}
+
+function finalizarPedido(){
+    let mensagem = "Olá, gostaria de fazer o pedido:"+"\n- Prato: " + prato + "\n- Bebida: " + bebida + "\n- Sobremesa: "+sobremesa+ "\nTotal: R$ " + total.toFixed(2);
+    window.open("https://wa.me/+5521999999999?text="+mensagem);
 }
 
